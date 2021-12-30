e0a9bc1fabf72ad32ed9c483da01046fb58d6cd9
subodh-stack <subodhpatil69@gmail.com>
2021-12-30 23:37:18 +0530

    Chnages added

--- src/main/java/Pojo/LaunchBrowser.java
+++ src/main/java/Pojo/LaunchBrowser.java
@@ -7,8 +7,6 @@ public class LaunchBrowser {
 	public WebDriver LaunchChrome(String url) {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get(url);
-		System.out.println("Browser Launched");
 		return driver;
 		
 }
