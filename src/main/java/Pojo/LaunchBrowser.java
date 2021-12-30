5f3b413b32450f5f20e01b6f0ac348cd0bcfbb93
91913 <91913@LAPTOP-P5TRHBSI>
2021-12-30 23:20:06 +0530

    Changed

--- src/main/java/Pojo/LaunchBrowser.java
+++ src/main/java/Pojo/LaunchBrowser.java
@@ -8,6 +8,8 @@ public class LaunchBrowser {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get(url);
+		System.out.println("Browser Launched");
 		return driver;
+		
 }
 }
